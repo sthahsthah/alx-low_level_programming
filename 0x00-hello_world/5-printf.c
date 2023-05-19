b412c8b820162309857573aee4c2342b4ed71015
sthahsthah <sthahstha16@gmail.com>
2023-05-19 05:05:47 -0700

    c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints a text
+ * Return: success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
