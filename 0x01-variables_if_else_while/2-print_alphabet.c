865ac033bf9b4d1b14b993da6370e8555857b27a
sthahsthah <sthahstha16@gmail.com>
2023-05-19 07:25:27 -0700

    if,else, variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		puchar('\n');
+	}
+	return (0);
+}
