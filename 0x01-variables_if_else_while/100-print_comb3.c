f97efbb3f7ac327ad0edb09f6cef87ee415b04b4
sthahsthah <sthahstha16@gmail.com>
2023-05-22 06:24:07 -0700

    two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,19 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 8; a++)
 	{
-	for (b = a + 1; b <= 9; b++)
-	{
-	putchar(a + '0');
-	putchar(b + '0');
+		for (b = a + 1; b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
 
-	putchar(',');
-	putchar(' ');
-	}
+			if (a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
