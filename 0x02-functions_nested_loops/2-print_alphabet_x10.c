9e572cceae3d8bcd3460c17d7648c6e88f30f4d7
sthahsthah <sthahstha16@gmail.com>
2023-05-23 05:12:45 -0700

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ * Description: Prints 10 times alphabet.
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a * 10);
+	}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 #define MAIN_H
 
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int _putchar(char c);
 #endif/*MAIN_H*/
