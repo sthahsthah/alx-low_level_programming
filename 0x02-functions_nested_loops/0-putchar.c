8f0bacc3024b7b98a3a887d2bcf5555cbccf7dab
sthahsthah <sthahstha16@gmail.com>
2023-05-23 04:09:55 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * description - Prints Print Putchar
+ * return - success
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+#endif/*MAIN_H*/
