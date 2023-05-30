9f1a1c8a1feca6ab621849f53f5b0e228dbe2b10
sthahsthah <sthahstha16@gmail.com>
2023-05-30 13:54:21 -0700

    pointrs

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: string to be printed
+ * return: string
+ */
+
+void _puts(char *str)
+{
+	 puts(str);
+}
