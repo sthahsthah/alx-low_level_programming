1738f5a51e72e9b7aea9d4d302d2f7c861f3dd9a
sthahsthah <sthahstha16@gmail.com>
2023-06-05 12:48:52 -0700

    wehh

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 #include <stddef.h>
+#include "main.h"
 
 /**
  * _strchr - locates a character in a string
  * @s: the string
  * @c: the character
  *
- * Return: a pointer to a character aka the character found
+ * Return: a pointer to a character.
  */
+
 char *_strchr(char *s, char c)
 {
 	int i;
